0ad6cec6fbf7c9a6bf509f3c653091cdf3842e6b
richaolas <ric_reborn@hotmail.com>
2020-02-12 16:19:49 +0800

    update

--- 20200212/loop_02_lights.py
+++ 20200212/loop_02_lights.py
@@ -1,15 +1,14 @@
 N, M = map(int, input().strip().split())
 
-lights = [True] * (N + 1)
+lights = [True] * N
 
 for m in range(M):
-    number = m + 1
-    for idx in range(1, N + 1):
-        if idx % number == 0:
+    for idx in range(N):
+        if (idx + 1) % (m + 1) == 0:
             lights[idx] = not lights[idx]
-
+print(lights)
 #result = list(filter(lambda x:not x, lights))
-result = [idx for idx in range(1, len(lights)) if not lights[idx]]
+result = [idx + 1 for idx, light in enumerate(lights) if not light]
 print(','.join(map(str, result)))
 
 
