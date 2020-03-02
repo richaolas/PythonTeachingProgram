9870ff9d3198b55bf83d0b8196347715d5bfeeb2
renjch <53853255+renjch@users.noreply.github.com>
2020-03-02 21:29:42 +0800

    Update tmp.py
    1

--- tmp.py
+++ tmp.py
@@ -1,15 +1,41 @@
 import random
-arr = []
-for i in range(10):
-    arr.append(random.randint(1, 5))
-print('随机数组',arr)
-arr.sort()
-arr2 = []
-for i in arr:
-    for j in arr2:
-        if arr.count(i) == int(j[2]):
-            break
-    else:
-        if str(i) + '_' +  str(arr.count(i)) not in arr2:
-            arr2.append(str(i) + '_' +  str(arr.count(i)))
-print('统计结果：', arr2)
+
+nums = [10, 7, 6, 6, 6, 5, 5, 5, 4, 2, 2, 1]
+
+cnts = []
+
+for i in range(len(nums)):
+    cnts.append(nums.count(nums[i]))
+
+print(cnts)
+cnts.sort()
+cnts.reverse()
+print(cnts[0])
+
+for i in range(len(nums)):
+    if nums.count(nums[i]) == cnts[0]:
+        print(nums[i])
+        #break
+
+
+# nums = []
+# for i in range(10):
+#     nums.append(random.randint(1, 10))
+#
+# print(nums)
+#
+# ### Question ?
+# nums.sort()
+#
+# print(nums)
+#
+# nums.reverse()
+#
+# print(nums)
+#
+# nums = [10, 9, 9, 8, 6, 6, 6, 4, 3, 1]
+#
+# print(nums.index(3))
+# print(nums.count(9))
+# print(nums.count(0))
+
