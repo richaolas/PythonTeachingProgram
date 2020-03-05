751ef68ebf424abc3f6a5611f32a18176ec9f0fb
renjch <53853255+renjch@users.noreply.github.com>
2020-03-05 21:25:30 +0800

    map
    map

---
+++ tmp.py
@@ -0,0 +1,76 @@
+# 建立一个字典， 里面有3个单词
+# create then print
+
+d={'go':'doh','mao':'cat'}
+d['yu']='fish'
+print(d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# d = {}
+# print(type(d))
+# d['男孩'] = 'boy'  # (key, value) ('男孩'，boy) d[key] = value
+# d['女孩'] = 'girl'
+# d['老师'] = 'teacher'
+# print(len(d))
+#
+# print(d['老师']) #dict[key]
+#
+# li = [1,2,3,4,5]
+# for item in li:
+#     print(item)
+#
+# for item in d:
+#     print(item, d[item])
+
+# s = {}
+# s['age'] = 18
+# s['name'] = '张山'
+# s['id'] = 2020001
+#
+# print(s)
+#
+# li = []
+# li = [1,2,3,4]
+#
+# s = {'age':18, 'name':'张山', 'id':2020001}
+# print(s)
+
+
+
+
+
+# li = []
+# print(type(li))
+# print(len(li)) #length
+# print(li + [1,2,3,]) # 1+ 2
+# print(li)
+#
+#
+# t = ()
+# print(type(t))
+# print(len(t))
+#
+# li = [1]
+# print(type(li))
+#
+# t = (1,)
+# print(type(t))
+#
+# t = (0, 1)
+# t[0] = 2
+
+
