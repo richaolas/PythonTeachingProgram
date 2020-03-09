2c31ffbd00913068f0145971397c64c92a2c8228
renjch <53853255+renjch@users.noreply.github.com>
2020-03-09 21:23:25 +0800

    Update tmp.py
    1

--- tmp.py
+++ tmp.py
@@ -1,36 +1,69 @@
-import random
+def j(x):
+    s = []
+    for i in x:
+        if i % 2 == 1:
+            s.append(i)
+    return s
 
-li = [1,2,3]
+print(j([1,2,34,5,6,7,7,76,76]))
 
-for i in li:
-    print(i)
+# def g(x):
+#     if x>=0:
+#         return x
+#     else:
+#         return -x
+# x=-10.5
+# print(g(x))
+#
+# def a(x):
+#     s=0
+#     for i in x:
+#         s+=i
+#     #len(x)
+#     return s/len(x)
+#
+# print(a([1,2,3,4,5]))
+# print(a({1:'3', 2:'8'}))
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 
 
-wordCount = 10
 
-while wordCount < 100:
-    #wordCount += random.randint(0, 10)
-    num = random.randint(0, 10)
-    wordCount = wordCount + num
-
-print('finally I learn %d' % (wordCount))
-
-
-isQuitButtonPressed = False
-
-while not isQuitButtonPressed:
-    print('play the game')
-    answer = input('Do you want play(y/n)?')
-    if answer == 'n':
-        isQuitButtonPressed = True
-
-
-random.randint(0, 10)
-
-def add(a, b):
-    c = a + b
-    return c
-
-result = add(100, 200)
-print(result)
-
+#
+#
+#
+#
+# import random
+#
+# def add(a, b):
+#     c = a + b
+#     return c
+#
+#
+# def getsum(a):
+#     s = 0
+#     for i in a:
+#         s += i
+#     return s
+#
+#
+# x = add(1, 2)
+# print(x)
+# y = add(24, 90)
+# print(y)
+#
+# s = getsum([1,2,3,4,5,6])
+# print(s)
+#
+# s = getsum((4,5,6,7,7,8))
+# print(s)
+#
+# add(random.randint(10,100), random.randint(9,80))
