57d8ae36ea6719cdf37bf928f11825f02f58003a
richaolas <ric_reborn@hotmail.com>
2020-04-25 10:01:44 +0800

    add draw_lollipop

--- test.py
+++ test.py
@@ -1,11 +1,24 @@
-import turtle
+# import turtle
+#
+# turtle.pencolor('#FF0000')
+# #turtle.pencolor('red')
+# #turtle.pencolor(255 / 255, 0 / 255, 0 / 255)
+# turtle.pensize(20)
+# turtle.forward(100)
+#
+# turtle.done()
+#
+# # 60,63,65
 
-turtle.pencolor('#FF0000')
-#turtle.pencolor('red')
-#turtle.pencolor(255 / 255, 0 / 255, 0 / 255)
-turtle.pensize(20)
-turtle.forward(100)
 
-turtle.done()
+# import turtle
+# #result = turtle.textinput('title', '提示')
+# #print(result)
+# turtle.circle(120,180,8)
+# ret = turtle.write('2+3=5', font=('Arial',18,))
+# print(ret)
+# turtle.stamp()
+# turtle.goto(110,0)
+# turtle.stamp()
+# turtle.done()
 
-# 60,63,65
