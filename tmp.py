e5734868ccf3d813772ab25d234daef88f451b78
renjch <53853255+renjch@users.noreply.github.com>
2020-04-26 08:55:58 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -36,3 +36,54 @@ star.close()
 
 
 
+import turtle
+
+turtle.speed(0)
+turtle.bgcolor('skyblue')
+w = turtle.window_width()
+h = turtle.window_height()
+
+turtle.fillcolor('blue')
+turtle.pencolor('blue')
+turtle.begin_fill()
+
+turtle.goto(-w/2, 0)
+turtle.forward(w)
+turtle.right(90)
+turtle.forward(h/2)
+turtle.right(90)
+turtle.forward(w)
+turtle.right(90)
+turtle.forward(h/2)
+
+turtle.end_fill()
+
+turtle.begin_fill()
+turtle.goto(-200, -10)
+turtle.goto(0, 100)
+turtle.setheading(180+20)
+turtle.circle(100, 180)
+turtle.goto(-200, -10)
+turtle.end_fill()
+
+turtle.begin_fill()
+turtle.goto(200, -10)
+turtle.goto(300, 50)
+turtle.setheading(180+20)
+turtle.circle(100, 180)
+turtle.goto(200, -10)
+turtle.end_fill()
+
+# turtle.begin_fill()
+# turtle.goto(-200, -250)
+# turtle.circle(400)
+# turtle.end_fill()
+# turtle.begin_fill()
+# turtle.goto(300, -350)
+# turtle.circle(400)
+# turtle.end_fill()
+
+turtle.done()
+
+
+
