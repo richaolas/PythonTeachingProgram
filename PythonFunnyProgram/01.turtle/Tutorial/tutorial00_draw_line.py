1060bb4d96199199d8fd1ee63037e2e006464c98
richaolas <ric_reborn@hotmail.com>
2020-06-22 20:54:33 +0800

    remove class by person, by content next step

---
+++ PythonFunnyProgram/01.turtle/Tutorial/tutorial00_draw_line.py
@@ -0,0 +1,20 @@
+# 1. turtle.pensize()   设置画笔大小
+# 2. turtle.forward()   前进
+# 3. turtle.done()      完成绘制，显示窗口
+# 
+
+import turtle
+
+#turtle.pensize(5)
+#turtle.forward(100)
+
+turtle.pensize(30)
+turtle.forward(120)
+turtle.pensize(20)
+turtle.forward(100)
+turtle.pensize(10)
+turtle.forward(80)
+turtle.pensize(15)
+turtle.forward(2)
+
+turtle.done()
