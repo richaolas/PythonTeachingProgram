ec9d499a0d0a02599ca93f7a800efa530a714e83
richaolas <ric_reborn@hotmail.com>
2021-01-03 19:43:34 +0800

    rebuild plane war game

---
+++ tmp.py
@@ -0,0 +1,25 @@
+import turtle
+
+turtle.pencolor('blue')
+turtle.dot(50)
+
+turtle.pu()
+turtle.forward(50)
+turtle.pd()
+
+turtle.pensize(50)
+
+turtle.pencolor('yellow')
+turtle.right(90)
+turtle.circle(-50,120)
+
+turtle.pencolor('green')
+turtle.circle(-50,120)
+
+turtle.pencolor('red')
+turtle.circle(-50,120)
+
+turtle.pencolor('yellow')
+turtle.dot(50)
+
+turtle.done()
