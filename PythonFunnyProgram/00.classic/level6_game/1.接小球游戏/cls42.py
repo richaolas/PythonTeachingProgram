bb30713a7a845567e1a701534bff048da8b9463e
richaolas <ric_reborn@hotmail.com>
2021-03-14 11:13:02 +0800

    update

--- PythonFunnyProgram/00.classic/level6_game/1.接小球游戏/cls42.py
+++ PythonFunnyProgram/00.classic/level6_game/1.接小球游戏/cls42.py
@@ -3,15 +3,31 @@ pygame 的基本使用
 '''
 
 import pygame
+
+WIDTH = 600
+HEIGHT = 500
+R = 25
+FPS = 30
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
 #初始化引擎
 pygame.init()
 
-screen = pygame.display.set_mode((600, 500))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-pos = [0, 100]
-
-def update():
-    pos[0] += 1
+ball = pygame.Surface((2*R, 2*R))
+ball.fill(BLACK)
+ball.set_colorkey(BLACK)
+pygame.draw.circle(ball, RED, (R,R), R)
+ball = ball.convert()
+rect = ball.get_rect()
+rect.midtop = (WIDTH/2, 0)
 
 while True:
     for event in pygame.event.get():
@@ -20,8 +36,9 @@ while True:
             pygame.quit()
     # 填充屏幕背景
     screen.fill((0,0,0))
-    update()
-    pygame.draw.circle(screen, (100, 100, 30), pos, 30, 0)
+    rect.y += 1
+    screen.blit(ball, rect)
+    # pygame.draw.circle(screen, (100, 100, 30), (100, 100), 30, 0)
     # 渲染屏幕
     pygame.display.update()
 
--- PythonFunnyProgram/00.classic/level6_game/1.接小球游戏/cls43.py
+++ PythonFunnyProgram/00.classic/level6_game/1.接小球游戏/cls43.py
@@ -5,27 +5,73 @@ pygame 显示文字
 
 import pygame
 
-pygame.init()
 WIDTH = 600
 HEIGHT = 500
+R = 25
+FPS = 30
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# game variable
 life = 3
 score = 0
 padding = 10
-lst = pygame.font.get_fonts()
-# print lst
-print(lst)
+
+# 初始化引擎
+pygame.init()
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-font = pygame.font.SysFont("microsoftyaheimicrosoftyaheiui", 25)
-#default_font = pygame.font.get_default_font()
-#font = pygame.font.SysFont(default_font, 24)
+# if using chinese
+# lst = pygame.font.get_fonts()
+# font = pygame.font.SysFont("microsoftyaheimicrosoftyaheiui", 25)
+
+font_name = pygame.font.match_font('arial')
+
+
+def draw_text(surf, text, size, x, y, color, align=0):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    if align == 0:  # left align
+        text_rect.topleft = (x, y)
+    elif align == 1:  # right align
+        text_rect.topright = (x, y)
+    elif align == 2:  # mid align
+        text_rect.midtop = (x, y)
+
+    surf.blit(text_surface, text_rect)
+
+
+ball = pygame.Surface((2 * R, 2 * R))
+ball.fill(BLACK)
+ball.set_colorkey(BLACK)
+pygame.draw.circle(ball, RED, (R, R), R)
+ball = ball.convert()
+rect = ball.get_rect()
+rect.midtop = (WIDTH / 2, 0)
+
 while True:
     for event in pygame.event.get():
+        print(event)
         if event.type == pygame.QUIT:
             pygame.quit()
+    # 填充屏幕背景
     screen.fill((0, 0, 0))
-    liftText = font.render("生命: {0}".format(life), True, (100, 200, 0))
-    scoreText = font.render("分数: {0}".format(score), True, (100, 200, 0))
-    screen.blit(liftText, (padding, padding))
-    screen.blit(scoreText, (WIDTH - padding - scoreText.get_width(), padding))
+    rect.y += 1
+    screen.blit(ball, rect)
+    # pygame.draw.circle(screen, (100, 100, 30), (100, 100), 30, 0)
+
+    # 渲染文字
+    draw_text(screen, "Life: {0}".format(life), 24, padding, padding, WHITE)
+    draw_text(screen, "Score: {0}".format(life), 24, WIDTH - padding, padding, WHITE, 1)
+
+    # 渲染屏幕
     pygame.display.update()
+
+# 有能力同学完成事件在屏幕显示程序
